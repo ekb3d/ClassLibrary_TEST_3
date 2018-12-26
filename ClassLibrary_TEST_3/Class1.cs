0d7e14c1fa44affc97fa779d9084b838906ca1f9
merkulov_vy <merkulov_vy@ADM-MVY>
2018-12-26 15:22:12 +0500

    Добавьте файлы проекта.

---
+++ ClassLibrary_TEST_3/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_TEST_3
+{
+    public class Class1
+    {
+    }
+}
